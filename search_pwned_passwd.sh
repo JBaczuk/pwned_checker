ef27fb4c18ed159836c2b3e51e8c0008970234b4
Jordan Baczuk <Jordan.Baczuk@gmail.com>
2018-06-10 00:55:02 -0600

    added script draft

---
+++ search_pwned_passwd.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "enter the password you want to search for:"
+read pwd
+hash=$(printf "$pwd" | openssl sha1 | tr '[:lower:]' '[:upper:]' | cut -c 10-)
+#echo $hash
+final_thirty_five=$(echo $hash | cut -c 6-)
+#echo $first_five
+first_five=$(echo $hash | rev | cut -c36- | rev)
+#echo $final_thirty_five
+# This takes forever, opting to use the API, which uses k-Anonymity and seems to be reasonable
+#awk '/$hash/' pwned-passwords-ordered-2.0.txt
+result=$(curl -sX GET https://api.pwnedpasswords.com/range/$first_five | grep $final_thirty_five)
+
+if [[ -z "$result" ]]; then
+    echo "Password is safe!"
+    exit 0
+else
+    num_hacks=${result#*:}
+    echo "Warning: The number of times \"password\" $pwd has been hacked:"
+    echo $num_hacks
+    exit 0
+fi
+
